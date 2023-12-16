f70361d2b904b11ef3e101cbc778aae06226f144
Fatima1964 <143363325+Fatima1964@users.noreply.github.com>
2023-12-15 18:16:19 -0700

    completed signin form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 
 
+
 import './App.css';
 
 const App = () => {
--- src/components/AuthComponent.js
+++ src/components/AuthComponent.js
@@ -1,78 +1,107 @@
-// AuthComponent.js
+/* AuthComponent.js */
+
 import React, { useState } from 'react';
 import './AuthComponent.css';
 
 const AuthComponent = () => {
+  // State to manage form data
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     password: '',
+    confirmPassword: '',
   });
 
+  // State to manage error and welcome messages
   const [errorMessage, setErrorMessage] = useState('');
   const [welcomeMessage, setWelcomeMessage] = useState('');
+
+  // State to manage sign-up/login toggle
   const [isSignUp, setIsSignUp] = useState(true);
 
+  // Handle input changes in the form
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  // Toggle between sign-up and login forms
   const handleFormToggle = () => {
-    setFormData({ name: '', email: '', password: '' });
+    setFormData({ name: '', email: '', password: '', confirmPassword: '' });
     setErrorMessage('');
     setWelcomeMessage('');
     setIsSignUp(!isSignUp);
   };
 
+  // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await fetch(
-        isSignUp
-          ? 'https://654d199b77200d6ba859fcf7.mockapi.io/SignIn'
-          : 'https://654d199b77200d6ba859fcf7.mockapi.io/Login',
-        {
+      // Check if user already exists for sign-up
+      const existingUser = await fetchUserByEmail(formData.email);
+
+      if (isSignUp) {
+        if (existingUser) {
+          throw new Error('Email already exists');
+        }
+
+        // Continue with sign-up logic
+        const response = await fetch('https://654d199b77200d6ba859fcf7.mockapi.io/SignIn', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify(formData),
+        });
+
+        if (!response.ok) {
+          throw new Error('Umable to sign up');
+        }
+      } else {
+        // Continue with login logic
+      }
+
+      // Common logic for both sign-up and login
+      setWelcomeMessage(isSignUp ? `Welcome, ${formData.name || formData.email}! Account created.` : 'Welcome back!');
+      setErrorMessage('');
+    } catch (error) {
+      console.error('Error:', error.message);
+      setErrorMessage(`Failed to ${isSignUp ? 'sign up' : 'log in'}. ${error.message}`);
+    }
+  };
+
+  // Fetch user data by email
+  const fetchUserByEmail = async (email) => {
+    try {
+      const response = await fetch(
+        `https://654d199b77200d6ba859fcf7.mockapi.io/SignIn?search=${encodeURIComponent(email)}`,
+        {
+          method: 'GET',
         }
       );
 
       if (!response.ok) {
-        throw new Error(isSignUp ? 'Failed to sign up' : 'Failed to log in');
-      }
-
-      const userDataResponse = await fetch('https://api.example.com/userdata', {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${response.token}`, // Include the token if needed
-        },
-      });
-
-      if (!userDataResponse.ok) {
         throw new Error('Failed to fetch user data');
       }
 
-      const userData = await userDataResponse.json();
-
-      setErrorMessage('');
-      setWelcomeMessage(`Welcome, ${userData.name || formData.name || formData.email}!`);
+      const userData = await response.json();
+      // Return a value (commented)
+      return userData.length ? userData[0] : null;
     } catch (error) {
-      console.error('Error:', error.message);
-      setErrorMessage(`Failed to ${isSignUp ? 'sign up' : 'log in'}. Please try again.`);
+      // Throw an error (commented)
+      throw new Error('Failed to fetch user data');
     }
   };
 
   return (
     <div className={`auth-container ${isSignUp ? 'signup' : 'login'}`}>
       <form onSubmit={handleSubmit} className="auth-form">
+        {/* Form title */}
         <h2>{isSignUp ? 'Sign Up' : 'Login'}</h2>
+
+        {/* Name input field for sign-up */}
         {isSignUp && (
           <div className="form-field">
-            <label htmlFor="name">Name:</label>
             <input
               type="text"
               id="name"
@@ -81,12 +110,14 @@ const AuthComponent = () => {
               onChange={handleInputChange}
               required
               autoComplete="username"
+              placeholder="Name"
+              
             />
           </div>
         )}
 
+        {/* Email input field */}
         <div className="form-field">
-          <label htmlFor="email">Email:</label>
           <input
             type="text"
             id="email"
@@ -94,11 +125,12 @@ const AuthComponent = () => {
             value={formData.email}
             onChange={handleInputChange}
             required
+            placeholder="Email"
           />
         </div>
 
+        {/* Password input field */}
         <div className="form-field">
-          <label htmlFor="password">Password:</label>
           <input
             type="password"
             id="password"
@@ -107,21 +139,45 @@ const AuthComponent = () => {
             onChange={handleInputChange}
             required
             autoComplete={isSignUp ? 'new-password' : 'current-password'}
+            placeholder="Password"
           />
         </div>
 
+        {/* Confirm Password input field for sign-up */}
+        {isSignUp && (
+          <div className="form-field">
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleInputChange}
+              required
+              autoComplete="new-password"
+              placeholder="Confirm Password"
+            />
+          </div>
+        )}
+
+        {/* Submit button */}
         <button type="submit">{isSignUp ? 'Sign Up' : 'Login'}</button>
       </form>
 
-      <p className="error-message">{errorMessage}</p>
-      {welcomeMessage && <p className="welcome-message">{welcomeMessage}</p>}
+      {/* Toggle between sign-up and login */}
+      {isSignUp && (
+        <p className="forgot-password">
+          Already have an account?{' '}
+          <button type="button" onClick={handleFormToggle}>
+            Login
+          </button>
+        </p>
+      )}
 
-      <p>
-        {isSignUp ? 'Already have an account? ' : "Don't have an account? "}
-        <button type="button" onClick={handleFormToggle}>
-          {isSignUp ? 'Login' : 'Sign Up'}
-        </button>
-      </p>
+      {/* Display error message */}
+      <p className="error-message">{errorMessage}</p>
+
+      {/* Display welcome message */}
+      {welcomeMessage && <p className="welcome-message">{welcomeMessage}</p>}
     </div>
   );
 };
