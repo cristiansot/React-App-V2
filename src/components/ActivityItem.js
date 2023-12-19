622ea9389c7db46fb5faffdaac8c49813760bc73
Novita H <novitahamilton@gmail.com>
2023-12-19 00:17:24 -0800

    Add image to some activity options to test
    - Create images > activities folders to store images used for activity options

--- src/components/ActivityItem.js
+++ src/components/ActivityItem.js
@@ -2,6 +2,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function ActivityItem({ activity, onDeleteActivity }) {
+  const imageSource = activity.image
+    ? require(`../images/activities/${activity.image}`)
+    : null;
+
   return (
     <div className="activity-item">
       <button
@@ -10,6 +14,7 @@ function ActivityItem({ activity, onDeleteActivity }) {
       >
         <FontAwesomeIcon icon={faTrash} />
       </button>
+      {imageSource && <img src={imageSource} alt={activity.activity} />}
       <p>Date: {activity && activity.date}</p>
       <p>Activity: {activity && activity.activity}</p>
       <p>Duration : {activity && activity.duration} </p>
